fa17eacb352078ed24485a3ad901fce298e1da70
wolcheg <3425866+Lycanthrope@users.noreply.github.com>
2021-07-03 12:14:13 +0300

    create empty solution with ping endpoint use docker compose

---
+++ src/ContentAssignmentService/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ContentAssignmentService.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthCheckController : ControllerBase
+	{
+		[HttpGet("ping")]
+		public string Ping()
+		{
+			return DateTime.UtcNow.ToString("O");
+		}
+	}
+}
